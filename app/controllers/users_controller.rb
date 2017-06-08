1cf7fa8237ab1e1a0cbeb5e2672acde34cac1d4b
澤村隆寛 <cawst222@gmail.com>
2017-06-08 21:36:26 +0900

    add account actication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,14 +16,14 @@ class UsersController < ApplicationController
   end
 
   def create
-   @user = User.new(user_params)
-   if @user.save
-     UserMailer.account_activation(@user).deliver_now
-     flash[:info] = "Please check your email to activate your account."
-     redirect_to root_url
-   else
-     render 'new'
-   end
+  @user = User.new(user_params)
+  if @user.save
+    @user.send_activation_email
+    flash[:info] = "Please check your email to activate your account."
+    redirect_to root_url
+  else
+    render 'new'
+  end
  end
 
  def edit
