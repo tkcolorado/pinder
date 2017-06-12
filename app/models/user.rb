9b21abc23a76e2a6518faa54ce5024dbe7ccf0c9
澤村隆寛 <cawst222@gmail.com>
2017-06-12 20:59:41 +0900

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -64,16 +72,33 @@ class User < ApplicationRecord
     end
 
 
-  # パスワード再設定の期限が切れている場合はtrueを返す
- def password_reset_expired?
-   reset_sent_at < 2.hours.ago
+    # パスワード再設定の期限が切れている場合はtrueを返す
+     def password_reset_expired?
+       reset_sent_at < 2.hours.ago
+     end
+
+     # ユーザーのステータスフィードを返す
+     def feed
+       Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+     end
+
+     # ユーザーのステータスフィードを返す
+ def feed
+   following_ids = "SELECT followed_id FROM relationships
+                    WHERE follower_id = :user_id"
+   Micropost.where("user_id IN (#{following_ids})
+                    OR user_id = :user_id", user_id: id)
  end
 
- # 試作feedの定義
-# 完全な実装は次章の「ユーザーをフォローする」を参照
-def feed
-  Micropost.where("user_id = ?", id)
-end
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
 
 
  private
