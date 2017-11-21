7087dcd9d3666c0d4ac6edf4eae3ef44777e647f
澤村隆寛 <cawst222@gmail.com>
2017-11-21 20:50:02 +0900

    検索拡張controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-  @users = User.paginate(page: params[:page])
+  @users = User.where(activated: true).paginate(page: params[:page]).search(prams[:search])
   end
 
   def show
