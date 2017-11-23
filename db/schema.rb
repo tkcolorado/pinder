8e0cae23a9ff2c973ad5873b90ffe365974fbd9e
澤村隆寛 <cawst222@gmail.com>
2017-11-23 14:16:43 +0900

    いいね。あとはajax

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122112205) do
+ActiveRecord::Schema.define(version: 20171123050229) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
