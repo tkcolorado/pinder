b725e9e9d12f8e183d50379dfc37910785777e22
澤村隆寛 <cawst222@gmail.com>
2017-12-06 18:07:25 +0900

    とりあえずlike部分を一旦消去

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :micropost, counter_cache: :likes_count
-  belongs_to :user
+  validates :user_id,{presence: true}
+  validates :post_id,{presence: true}
 end
